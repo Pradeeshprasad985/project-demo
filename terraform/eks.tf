2500b79379d1ea8cf63a6ec4c39477cb4bc0d91b
pradeesh prasad <pradeeshprassad985@gmail.com>
2026-04-07 13:40:09 +0530

    Upgrade EKS module version to 20.0

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,11 +1,12 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 19.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
   cluster_name    = "demo-eks"
   cluster_version = "1.29"
 
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
